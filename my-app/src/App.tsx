4e23f5f3c64f2bfb1c0bdf2768bd6171ff4d9af9
Saltyy <clement.bouret@epitech.eu>
2025-09-02 14:38:29 +0200

    [Feat/App] using layout now

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,14 +1,18 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./Login";
 import Register from "./Register";
+//import Dashboard from "./Dashboard";
+//import Profile from "./Profile";
+import Layout from "@/components/Layout";
 
 export default function App() {
   return (
     <Routes>
-      {/* Par défaut → /login */}
-      <Route path="/" element={<Navigate to="/login" />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
+      <Route element={<Layout />}>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Route>
     </Routes>
   );
 }
